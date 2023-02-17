b3d1acce7c3e23fe61f5729b93f59827166aeecc
Hillary Chiamaka <hillarychiamaka47@gmail.com>
2023-02-16 20:20:26 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints a string followed by a new line
+ * main- prints a string followed by a new line
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
